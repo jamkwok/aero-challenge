99819b3ad95799ea752be234ce75f32c54b0803f
James Kwok <jameskwok@Jamess-MacBook-Pro.local>
2020-10-23 11:38:20 +1100

    split out validation into file

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -4,9 +4,10 @@ import decodeJwt from '../util/decodeJwt';
 import express from 'express';
 import { getUsers, postUsers } from '../application/users';
 import logger from '../util/logger';
-import { body, validationResult } from 'express-validator';
+import { validationResult } from 'express-validator';
 import { BadRequestException } from '../util/errors';
 import { NewUser } from '../application/models/NewUser';
+import { postUsersValidation } from './validation/usersValidation';
 
 const router = express.Router();
 
@@ -27,31 +28,7 @@ router.get(
 router.post(
   '/users',
   decodeJwt,
-  [
-    body().isArray().notEmpty(),
-    body('*.name', 'name must be a string')
-      .exists()
-      .notEmpty()
-      .isString()
-      .trim()
-      .escape(), // Sanitization
-    body('*.email', 'email field must be an email').exists().isEmail().normalizeEmail(),
-    body('*.meta.isVerified', 'meta.isVerified field must be boolean').custom((value) => {
-      // .optional({falsey: true}) typescript bug
-      if (value === undefined || typeof value === 'boolean') return true;
-      throw new Error('isVerified must be boolean when set');
-    }),
-    body('*.meta.isExpired', 'meta.isExpired field must be boolean').custom((value) => {
-      if (value === undefined || typeof value === 'boolean') return true;
-      throw new Error('isExpired must be boolean when set');
-    }),
-    body('*.meta.addedOn', 'meta.addedOn field must be a string')
-      .exists()
-      .notEmpty()
-      .isString()
-      .trim()
-      .escape() // Sanitization
-  ],
+  postUsersValidation,
   async (req: Request, res: Response, next: NextFunction): Promise<any> => {
     try {
       const errors = validationResult(req);
---
+++ src/controllers/validation/usersValidation.ts
@@ -0,0 +1,26 @@
+import { body } from 'express-validator';
+export const postUsersValidation = [
+    body().isArray().notEmpty(),
+    body('*.name', 'name must be a string')
+      .exists()
+      .notEmpty()
+      .isString()
+      .trim()
+      .escape(), // Sanitization
+    body('*.email', 'email field must be an email').exists().isEmail().normalizeEmail(),
+    body('*.meta.isVerified', 'meta.isVerified field must be boolean').custom((value) => {
+      // .optional({falsey: true}) typescript bug
+      if (value === undefined || typeof value === 'boolean') return true;
+      throw new Error('isVerified must be boolean when set');
+    }),
+    body('*.meta.isExpired', 'meta.isExpired field must be boolean').custom((value) => {
+      if (value === undefined || typeof value === 'boolean') return true;
+      throw new Error('isExpired must be boolean when set');
+    }),
+    body('*.meta.addedOn', 'meta.addedOn field must be a string')
+      .exists()
+      .notEmpty()
+      .isString()
+      .trim()
+      .escape() // Sanitization
+  ];
